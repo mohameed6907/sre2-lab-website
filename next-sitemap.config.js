0156e66573301ed41e15e0c5f7cf9fe0c8775e4b
Mohammed Gamal <mohameed6907@gmail.com>
2026-09-24 21:03:54 +0300

    fix: upgrade next.js and react dependencies to resolve security vulnerability

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,4 +3,5 @@ module.exports = {
   siteUrl: process.env.SITE_URL || "https://sre2-lab.vercel.app",
   generateRobotsTxt: true,
   generateIndexSitemap: false,
+  exclude: ["/icon.png", "/apple-icon.png"],
 };
